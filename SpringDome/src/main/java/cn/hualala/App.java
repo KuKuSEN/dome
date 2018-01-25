499acad7dc92f90361ea50c234282a7a9ec08b12
gaosentian <17610990332@163.com>
2018-01-25 22:34:29 +0800

    doem

---
+++ SpringDome/src/main/java/cn/hualala/App.java
@@ -0,0 +1,12 @@
+package cn.hualala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
